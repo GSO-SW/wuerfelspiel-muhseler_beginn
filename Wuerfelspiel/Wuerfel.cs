a29ab158742b659d579d032d61271b4d33d274f9
Stranger0206 <dustin.beginn@outlook.de>
2022-03-30 13:03:18 +0200

    Erster Commit

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,5 +4,30 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
+        private int anzahlSeiten;
+        private int letztesErgebnis;
+        private bool gesichert;
+        private Random rnd;
+
+        Wuerfel()
+        {
+        }
+
+        Wuerfel(int anzahlSeiten)
+        {
+            this.anzahlSeiten = anzahlSeiten;
+        }
+
+        public int AnzahlSeiten
+        {
+            get;
+        }
+
+        public int LetztesErgebnis
+        {
+            get;
+        }
+
+
     }
 }
